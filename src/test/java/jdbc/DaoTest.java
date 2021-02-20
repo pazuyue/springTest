b1956ed80fdabe819c6838d323ac263508ba41f5
yueguang <yueguang@qingmutec.com>
2021-02-20 21:42:12 +0800

    Spring 测试注解

--- src/test/java/jdbc/DaoTest.java
+++ src/test/java/jdbc/DaoTest.java
@@ -3,6 +3,7 @@ package jdbc;
 
 import com.ssm.aspectj.Model.User;
 import com.ssm.aspectj.UserDao;
+import com.ssm.aspectj.controller.Profile;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,6 +15,12 @@ public class DaoTest {
         UserDao userDao = (UserDao) applicationContext.getBean("userDao");
         User user = userDao.findUserById(1);
         System.out.println(user);
+        User user2 = userDao.findUserById(1);
+        System.out.println(user2);
+
+      /*  Profile profile = (Profile) applicationContext.getBean("profile");
+        profile.printAge();
+        profile.printName();*/
 
     }
 }
